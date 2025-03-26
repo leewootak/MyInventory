1a2356e34a3a769ee8698443a83c9805a6c5cd07
leewootak <leewt32414@gmail.com>
2025-03-26 12:23:49 +0900

    [feat] 리소스 추가

--- Assets/1. Scripts/Manager/GameManager.cs
+++ Assets/1. Scripts/Manager/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Versioning;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -23,6 +24,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        Application.targetFrameRate = 60;
         SetData();
     }
 
